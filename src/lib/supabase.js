efee411f474d4ee85ad1989b72103096027fe6ae
Isotope <101206524+realisotope@users.noreply.github.com>
2025-12-14 19:08:04 +0000

    Update supabase.js
    reduce api calls

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,11 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://edcfmaofcobqzmsylcfv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkY2ZtYW9mY29icXptc3lsY2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzUxMzUsImV4cCI6MjA4MTExMTEzNX0.BrT8PIDpVnkVrDVLKr9K7WPoIrnBGgSzstKxEA6U_Cs"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkY2ZtYW9mY29icXptc3lsY2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzUxMzUsImV4cCI6MjA4MTExMTEzNX0.BrT8PIDpVnkVrDVLKr9K7WPoIrnBGgSzstKxEA6U_Cs",
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+    },
+  }
 );
